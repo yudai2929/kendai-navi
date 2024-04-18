fb08912b4f5938f610cc30d6dc67c02264f0b7a1
yudai2929 <yudai201729@icloud.com>
2024-04-18 15:04:43 +0900

    👍feat: error handling

---
+++ backend/pkg/lib/errors/errors.go
@@ -0,0 +1,132 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"runtime/debug"
+
+	"firebase.google.com/go/v4/auth"
+	"github.com/go-playground/validator/v10"
+)
+
+type ErrorCode int
+
+const (
+	Unknown ErrorCode = iota
+	InvalidArgument
+	NotFound
+	AlreadyExists
+	Internal
+	Unauthenticated
+	PermissionDenied
+	Unimplemented
+	Unavailable
+)
+
+func (c ErrorCode) String() string {
+	switch c {
+	case Unknown:
+		return "Unknown"
+	case InvalidArgument:
+		return "InvalidArgument"
+	case NotFound:
+		return "NotFound"
+	case AlreadyExists:
+		return "AlreadyExists"
+	case Internal:
+		return "Internal"
+	case Unauthenticated:
+		return "Unauthenticated"
+	case PermissionDenied:
+		return "PermissionDenied"
+	case Unimplemented:
+		return "Unimplemented"
+	case Unavailable:
+		return "Unavailable"
+	default:
+		return "Unknown"
+	}
+}
+
+type customError struct {
+	code   ErrorCode
+	origin error
+	stack  string
+}
+
+func (ce *customError) Error() string {
+	return ce.code.String()
+}
+
+func New(code ErrorCode) error {
+	return &customError{
+		code:   code,
+		origin: fmt.Errorf(code.String()),
+		stack:  string(debug.Stack()),
+	}
+}
+
+func Newf(code ErrorCode, format string, args ...interface{}) error {
+	return &customError{
+		code:   code,
+		origin: fmt.Errorf(format, args...),
+		stack:  string(debug.Stack()),
+	}
+}
+
+func Wrap(err error) error {
+	ce := &customError{}
+	if errors.As(err, &ce) {
+		return err
+	}
+	return ce.convert(err, err, ce.stack)
+}
+
+func Wrapf(err error, format string, args ...interface{}) error {
+	ce := &customError{}
+	if As(err, &ce) {
+		return err
+	}
+	return ce.convert(err, fmt.Errorf(format, args...), ce.stack)
+}
+
+func (cs *customError) convert(err error, origin error, stack string) error {
+	cs.origin = origin
+	cs.stack = stack
+
+	// Validator
+	var ve validator.ValidationErrors
+	if As(err, &ve) {
+		cs.code = InvalidArgument
+		return cs
+	}
+
+	// Firebase Auth
+	if auth.IsEmailNotFound(err) {
+		cs.code = NotFound
+		return cs
+	}
+	if auth.IsEmailAlreadyExists(err) {
+		cs.code = AlreadyExists
+		return cs
+	}
+
+	cs.code = Internal
+	return cs
+}
+
+func EqualCode(err error, code ErrorCode) bool {
+	e, ok := err.(*customError)
+	if !ok {
+		return false
+	}
+	return e.code == code
+}
+
+func As(err error, target interface{}) bool {
+	return errors.As(err, target)
+}
+
+func Is(err, target error) bool {
+	return errors.Is(err, target)
+}
---
+++ backend/pkg/lib/errors/errors_test.go
@@ -0,0 +1,267 @@
+package errors
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/zeebo/assert"
+)
+
+func TestNew(t *testing.T) {
+	type args struct {
+		code ErrorCode
+	}
+	type output struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want output
+	}{
+		{
+			name: "ok",
+			args: args{
+				code: InvalidArgument,
+			},
+			want: output{
+				err: &customError{
+					code:   InvalidArgument,
+					origin: fmt.Errorf("InvalidArgument"),
+					stack:  "",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, New(tt.args.code).Error(), tt.want.err.Error())
+		})
+	}
+
+}
+
+func TestNewf(t *testing.T) {
+	type args struct {
+		code   ErrorCode
+		format string
+		args   []interface{}
+	}
+	type output struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want output
+	}{
+		{
+			name: "ok",
+			args: args{
+				code:   InvalidArgument,
+				format: "test",
+				args:   []interface{}{"test"},
+			},
+			want: output{
+				err: &customError{
+					code:   InvalidArgument,
+					origin: fmt.Errorf("test"),
+					stack:  "",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, Newf(tt.args.code, tt.args.format, tt.args.args...).Error(), tt.want.err.Error())
+		})
+	}
+}
+
+func TestWrap(t *testing.T) {
+	type args struct {
+		err error
+	}
+	type output struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want output
+	}{
+		{
+			name: "ok",
+			args: args{
+				err: fmt.Errorf("test"),
+			},
+			want: output{
+				err: &customError{
+					code:   Internal,
+					origin: fmt.Errorf("test"),
+					stack:  "",
+				},
+			},
+		},
+		{
+			name: "ok: エラーを二重にラップ",
+			args: args{
+				err: Wrap(fmt.Errorf("test")),
+			},
+			want: output{
+				err: &customError{
+					code:   Internal,
+					origin: fmt.Errorf("test"),
+					stack:  "",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, Wrap(tt.args.err).Error(), tt.want.err.Error())
+		})
+	}
+}
+
+func TestWrapf(t *testing.T) {
+	type args struct {
+		err    error
+		format string
+		args   []interface{}
+	}
+	type output struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want output
+	}{
+		{
+			name: "ok",
+			args: args{
+				err:    fmt.Errorf("test"),
+				format: "test",
+				args:   []interface{}{"test"},
+			},
+			want: output{
+				err: &customError{
+					code:   Internal,
+					origin: fmt.Errorf("test"),
+					stack:  "",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, Wrapf(tt.args.err, tt.args.format, tt.args.args...).Error(), tt.want.err.Error())
+		})
+	}
+}
+
+func TestEqualCode(t *testing.T) {
+	type args struct {
+		err  error
+		code ErrorCode
+	}
+	type output struct {
+		ok bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want output
+	}{
+		{
+			name: "ok",
+			args: args{
+				err:  New(InvalidArgument),
+				code: InvalidArgument,
+			},
+			want: output{
+				ok: true,
+			},
+		},
+		{
+			name: "ng",
+			args: args{
+				err:  New(InvalidArgument),
+				code: NotFound,
+			},
+			want: output{
+				ok: false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, EqualCode(tt.args.err, tt.args.code), tt.want.ok)
+		})
+	}
+}
+
+func TestIs(t *testing.T) {
+	var ErrMyError = New(InvalidArgument)
+	type args struct {
+		err    error
+		target error
+	}
+	type output struct {
+		ok bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want output
+	}{
+		{
+			name: "ok",
+			args: args{
+				err:    ErrMyError,
+				target: Wrapf(ErrMyError, "test"),
+			},
+			want: output{
+				ok: true,
+			},
+		},
+		{
+			name: "ng",
+			args: args{
+				err:    New(InvalidArgument),
+				target: fmt.Errorf("test"),
+			},
+			want: output{
+				ok: false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, Is(tt.args.err, tt.args.target), tt.want.ok)
+		})
+	}
+}
+
+func TestCustomError_convert(t *testing.T) {
+	t.Run("valid error", func(t *testing.T) {
+		validate := validator.New()
+
+		type input struct {
+			Name string `validate:"required"`
+		}
+		err := validate.Struct(input{})
+
+		ce := &customError{}
+		converted := ce.convert(err, err, "")
+		assert.Equal(t, converted.(*customError).code, InvalidArgument)
+	})
+}
