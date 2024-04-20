a79ef62ad7a1869d999d6c8f9487d37ad5f6dfd1
yudai2929 <yudai201729@icloud.com>
2024-04-20 13:23:27 +0900

    👍feat: init ent

---
+++ backend/db/ent/schema/user.go
@@ -0,0 +1,36 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").Unique(),
+		field.String("nickname"),
+		field.String("firebase_uid"),
+		field.String("email"),
+		field.String("profile_image_url"),
+		field.Time("created_at"),
+		field.Time("updated_at"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
+
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("email").Unique(),
+	}
+}
